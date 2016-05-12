14e563289dfd59cc196a81f39e8e9fda878e4dcc
Angelo G. Del Regno <kholk11@gmail.com>
2016-05-12 13:47:43 +0200

    arm: MSM: Add MSM8930 Fame board

---
+++ arch/arm/mach-msm/include/mach/board_fame.h
@@ -0,0 +1,24 @@
+/*
+ * Nokia Lumia 520 (Fame) Platform header
+ *
+ * Copyright (C) 2016, AngeloGioacchino Del Regno <kholk11@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __ARCH_ARM_MACH_MSM_BOARD_FAME_H
+#define __ARCH_ARM_MACH_MSM_BOARD_FAME_H
+
+#define FAME_CMA_HI_START_ADDR		0x9c000000
+#define FAME_CMA_HI_END_ADDR		0xa0000000
+
+#define FAME_CMA_LO_START_ADDR		0x97e00000
+
+#endif
